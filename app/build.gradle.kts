e75c496fa2c220cdc2f11e3332f5832c6da60133
LongEP <2115513882@qq.com>
2026-08-30 02:13:22 +0800

    bump to 2.4.1 @可丽饼

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "homes.gensokyo.enigma"
         minSdk = 24
         targetSdk = 34
-        versionCode = 42
-        versionName = "2.4.0"
+        versionCode = 43
+        versionName = "2.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         versionNameSuffix = "@可丽饼"
